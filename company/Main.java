7ed1e8655a1b2d85c5622f9ea9fb469b5f47fd67
Luckylake215 <70714955+Luckylake215@users.noreply.github.com>
2020-12-03 15:24:14 +0300

    Add files via upload

---
+++ company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main
+{
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args)
+    {
+        ArrayList<Employee> staff = loadStaffFromFile();
+        staff.forEach(System.out::println);
+
+        staff.sort((n1, n2) -> {if (n1.getName().compareTo(n2.getName()) == 0)
+        {
+            return n1.getSalary().compareTo(n2.getSalary());
+        }
+        else
+            {
+            return n1.getName().compareTo(n2.getName());
+        }});
+        System.out.println();
+        staff.forEach(System.out::println);
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile()
+    {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try
+        {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for(String line : lines)
+            {
+                String[] fragments = line.split("\t");
+                if(fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
